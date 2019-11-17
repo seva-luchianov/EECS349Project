567027e85d3965db4a6749f6e0991de261c14eb7
Seva Luchianov <seva.luchianov@everykey.com>
2019-11-16 19:29:36 -0500

    Can parse all posts in forum, added stats

--- src/js/background.js
+++ src/js/background.js
@@ -1,50 +1,88 @@
-let articles = [];
-
 console.log("Running...");
 
-let tabQueue = [];
+let articles = {};
 
 const onMessage = {
-    "save-article": function(data) {
-        for (const article of articles) {
-            if (article.title === data.title) {
-                return;
-            }
-        }
-
-        console.log("Add new article", data.title);
-        articles.push(data);
-
-        console.log(articles);
+    "save-posts": function(data) {
+        let posts = articles[data.tid].posts;
+        posts = posts.concat(data.posts);
+        articles[data.tid].posts = posts;
     },
-    "open-tab": function(data) {
+    "parse-article": function(data) {
         console.log("queue tab", data);
-        tabQueue.push("https://hackforums.net/" + data);
+        articles[data.tid] = {
+            url: data.url,
+            tid: data.tid,
+            title: data.title,
+            posts: []
+        };
+        chrome.tabs.create({
+            url: data.url
+        });
+    },
+    "finish-posts": function(data, sender) {
+        chrome.tabs.remove(sender.tab.id);
     }
 };
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log(request);
-    onMessage[request.command](request.data);
+    onMessage[request.command](request.data, sender);
 });
 
-setInterval(function() {
-    if (tabQueue.length) {
-        let url = tabQueue.shift();
-        console.log("Open new tab", url);
-        chrome.tabs.create({
-            url: url
-        });
-    }
-}, 5000);
+chrome.browserAction.onClicked.addListener(function() {
+    stats();
+    console.log("articles", articles);
+    console.log(JSON.stringify(articles));
+});
 
 function stats() {
     authors = {};
-    for (const article of articles) {
+    Object.entries(articles).forEach(function(entry) {
         // do stats
-        incremenentVal(authors, article.posts[0].author.name);
+        entry[1].price = 0;
+        let posts = entry[1].posts;
+        if (!posts.length) {
+            return;
+        }
+        incremenentVal(authors, posts[0].author.name);
+        for (let i = 1; i < posts.length; i++) {
+            let post = posts[i];
+            post.review = calculateReview(post);
+            post.qa = calculateQA(post);
+            post.trade = calculateTrade(post);
+        }
+    });
+    console.log("authors", authors);
+    console.log(JSON.stringify("authors"));
+}
+
+function calculateReview(post) {
+    return searchRegex(
+        post.content, ["good", "nice", "works", "great", "success", "wonderful"]
+    ) - searchRegex(
+        post.content, ["bad", "cannot", "problem", "trouble", "issue"]
+    );
+}
+
+function calculateQA(post) {
+    return post.content.includes("? ") || post.content.indexOf("?") >= post.content.length - 2;
+}
+
+function calculateTrade(post) {
+    return searchRegex(post.content, ["trade, exchange, share"]);
+}
+
+function searchRegex(searchString, tagMatches, enforceBoundaries) {
+    let matches = 0;
+    for (let i = 0; i < tagMatches.length; i++) {
+        let regex = tagMatches[i];
+        if (enforceBoundaries) {
+            regex = '\\b' + tagMatches[i] + '\\b';
+        }
+        matches += (searchString.toLowerCase().match(new RegExp(regex, 'g')) || []).length;
     }
-    console.log(authors);
+    return matches;
 }
 
 function incremenentVal(obj, val) {
@@ -54,7 +92,3 @@ function incremenentVal(obj, val) {
         obj[val] = 1;
     }
 }
-
-chrome.storage.local.set({
-    articles: articles
-});
--- src/js/content.js
+++ src/js/content.js
@@ -1,55 +1,81 @@
 const $ = require('jquery');
+const qs = require('query-string');
 
 if (location.pathname === "/showthread.php") {
-    const $article = $(document).find('article');
+    let q = qs.parse(location.search);
+    console.log("parsed query string", q);
 
-    let data = {
-        title: $article.find('.thead').text().trim(),
-        posts: function() {
-            let posts = [];
-            let $posts = $article.find("#posts").children("div");
-            for (let i = 0; i < $posts.length; i++) {
-                let $post = $($posts[i]);
-                posts.push({
-                    author: {
-                        name: $post.find(".author_information").find(".largetext").text().trim(),
-                        stats: {
-                            posts: "",
-                            threads: "",
-                            bRating: "",
-                            popularity: "",
-                            bytes: "",
-                            gamexp: ""
-                        }
-                    },
-                    content: $post.find(".post_content").text().trim(),
-                    id: $post.attr("id")
-                });
+    let currPage = document.getElementsByClassName("pagination_current")[0].innerText * 1;
+    if (currPage != q.page) {
+        console.log("reached end of thread!");
+        chrome.runtime.sendMessage({
+            command: "finish-posts",
+            data: {
+                tid: q.tid
             }
-            return posts;
-        }()
-    };
-
-    console.log(data);
-
-    chrome.runtime.sendMessage({
-        command: "save-article",
-        data: data
-    });
-
-    if ( /* last page */ ) {
+        });
         window.close();
     } else {
-        location.href = "&page" + currpage + 1;
-    }
+        const $article = $(document).find('article');
 
+        let data = {
+            tid: q.tid,
+            posts: function() {
+                let posts = [];
+                let $posts = $article.find("#posts").children("div");
+                for (let i = 0; i < $posts.length; i++) {
+                    let $post = $($posts[i]);
+                    posts.push({
+                        author: {
+                            name: $post.find(".author_information").find(".largetext").text().trim(),
+                            stats: function() {
+                                const statKeys = ["posts", "threads", "bRating", "popularity", "bytes", "gamexp"];
+                                let stats = {};
+                                let $stats = $post.find(".author_statistics").find(".author_row");
+                                for (let j = 0; j < $stats.length; j++) {
+                                    const $stat = $($stats[j]).find(".author_value");
+                                    stats[statKeys[j]] = $stat.text().trim();
+                                }
+                                return stats;
+                            }()
+                        },
+                        content: $post.find(".post_content").text().trim(),
+                        id: $post.attr("id")
+                    });
+                }
+                return posts;
+            }()
+        };
+
+        console.log(data);
+
+        chrome.runtime.sendMessage({
+            command: "save-posts",
+            data: data
+        });
+
+        q.page++;
+        console.log("next query string", qs.stringify(q));
+        setTimeout(function() {
+            location.search = qs.stringify(q);
+        }, 500);
+    }
 } else if (location.pathname === "/forumdisplay.php") {
     let $articles = $(document).find(".subject_old,.subject_new");
     for (let i = 0; i < $articles.length; i++) {
         let $article = $($articles[i]);
+        let $link = $article.find("a");
+        let $stats = $article.closest("tr").children();
+
         chrome.runtime.sendMessage({
-            command: "open-tab",
-            data: $article.find("a").attr("href")
+            command: "parse-article",
+            data: {
+                url: "https://hackforums.net/" + $link.attr("href") + "&page=1",
+                tid: $article.attr("id").substr(4),
+                title: $link.text().trim(),
+                replies: $stats[2].innerText,
+                views: $stats[3].innerText
+            }
         });
     }
 } else {
